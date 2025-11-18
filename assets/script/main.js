9bd8f6b8191a7776202538327a09880690ce691c
miyu-togano <koto0417.mi@gmail.com>
2025-11-18 16:51:29 +0900

    CSS-2

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,17 +1,25 @@
+// Slick//
 $('.voices-slider').slick({
+  arrows:true,
   dots: true,
   infinite: false,
   speed: 300,
   slidesToShow: 3,
   slidesToScroll: 3,
-  responsive: [
-    {
-      breakpoint: 767.99,
+  
+
+  responsive: [{
+      breakpoint: 767.9,
       settings: {
         slidesToShow: 1,
         slidesToScroll: 1,
+        arrows:true,
         dots: true
       }
-    },
-  ]
+    }]
+});
+
+$('.accordion-title').on('click', function (e) {
+  $(this).toggleClass("active");
+  $(this).next().slideToggle();
 });
