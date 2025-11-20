3922a80a1490a956dd264d85dac3efad8f523bdb
miyu-togano <koto0417.mi@gmail.com>
2025-11-20 16:29:17 +0900

    CSS-3

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,22 +1,25 @@
 // Slick//
 $('.voices-slider').slick({
-  arrows:true,
+  arrows: true,
+  prevArrow: '<img src="/assets/image/arrow-left-circle-fill_pc.svg" class="slide-arrow prev-arrow">',
+  nextArrow: '<img src="/assets/image/arrow-right-circle-fill_pc.svg" class="slide-arrow next-arrow">',
   dots: true,
   infinite: false,
-  speed: 300,
   slidesToShow: 3,
   slidesToScroll: 3,
-  
+
 
   responsive: [{
-      breakpoint: 767.9,
-      settings: {
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        arrows:true,
-        dots: true
-      }
-    }]
+    breakpoint: 767.9,
+    settings: {
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      arrows: true,
+      prevArrow: '<img src="/assets/image/arrow-left-circle-fill_sp.svg" class="slide-arrow prev-arrow">',
+      nextArrow: '<img src="/assets/image/arrow-right-circle-fill_sp.svg" class="slide-arrow next-arrow">',
+      dots: true
+    }
+  }]
 });
 
 $('.accordion-title').on('click', function (e) {
