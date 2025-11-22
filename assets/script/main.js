891e6c2bc7ed5a48725eae87b68b6048173d9dd8
miyu-togano <koto0417.mi@gmail.com>
2025-11-22 22:47:34 +0900

    CSS修正

--- assets/script/main.js
+++ assets/script/main.js
@@ -1,14 +1,15 @@
-// Slick//
+// Slick //
 $('.voices-slider').slick({
   arrows: true,
   prevArrow: '<img src="/assets/image/arrow-left-circle-fill_pc.svg" class="slide-arrow prev-arrow">',
   nextArrow: '<img src="/assets/image/arrow-right-circle-fill_pc.svg" class="slide-arrow next-arrow">',
   dots: true,
   infinite: false,
-  variableWidth:true,
+  variableWidth:false,
+  centerMode: false,
+  adaptiveHeight: false,
   slidesToShow: 3,
   slidesToScroll: 3,
-
 
   responsive: [{
     breakpoint: 767.9,
@@ -23,7 +24,27 @@ $('.voices-slider').slick({
   }]
 });
 
+
+// accordion //
 $('.accordion-title').on('click', function (e) {
   $(this).toggleClass("active");
   $(this).next().slideToggle();
+});
+
+
+// scrolltop //
+$(function () {
+const pageTop = $(".scroll-top-btn");
+
+$(window).scroll(function () {
+  if ($(this).scrollTop() > 80) {
+    $(".scroll-top-btn").fadeIn(300).css("display","flex");
+  } else {
+    $(".scroll-top-btn").fadeOut(300);
+  }
+});
+
+$(".scroll-top-btn").click(function () {
+  $("html, body").animate({ scrollTop: 0 }, 500);
+});
 });
